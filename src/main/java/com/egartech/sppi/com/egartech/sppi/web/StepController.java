f4422a00fdc53c44e333165d0ac6faa4f8ae8537
Alander <Alander@tzoom.pro>
2018-08-30 12:26:33 +0000

    start view for step

---
+++ src/main/java/com/egartech/sppi/com/egartech/sppi/web/StepController.java
@@ -0,0 +1,16 @@
+package com.egartech.sppi.com.egartech.sppi.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class StepController {
+    
+    @RequestMapping(value="step", method=RequestMethod.GET)
+    public ModelAndView getStepPage() {
+        return new ModelAndView("step");
+    }
+    
+}
