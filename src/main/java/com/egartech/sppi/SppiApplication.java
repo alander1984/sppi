4879643f8e19741b37aa4c7302b2cb1ceffab90c
a.hrenov <a.hrenov@egartech.com>
2018-08-30 15:51:09 +0400

    Initial

---
+++ src/main/java/com/egartech/sppi/SppiApplication.java
@@ -0,0 +1,12 @@
+package com.egartech.sppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppiApplication.class, args);
+	}
+}
