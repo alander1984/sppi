6c3997419d3b31c81b86c3123629232a420809e1
Alander <Alander@tzoom.pro>
2018-08-30 12:04:46 +0000

    start page

--- src/main/java/com/egartech/sppi/com/egartech/sppi/web/HomeController.java
+++ src/main/java/com/egartech/sppi/com/egartech/sppi/web/HomeController.java
@@ -1,4 +1,16 @@
 package com.egartech.sppi.com.egartech.sppi.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ModelAndView getHomeView() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
 }
