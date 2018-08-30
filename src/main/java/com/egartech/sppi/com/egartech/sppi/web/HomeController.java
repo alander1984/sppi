cb87ecf5a1f7b6aa3fd7726d20f5345294cb1995
afedorenko <a.fedorenko@egartech.com>
2018-08-30 17:00:21 +0400

    start page

--- src/main/java/com/egartech/sppi/com/egartech/sppi/web/HomeController.java
+++ src/main/java/com/egartech/sppi/com/egartech/sppi/web/HomeController.java
@@ -1,5 +1,6 @@
 package com.egartech.sppi.com.egartech.sppi.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,5 +13,11 @@ public class HomeController {
     public ModelAndView getHomeView() {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
+    }
+
+    @RequestMapping(value="/start_process", method = RequestMethod.GET)
+    public HttpStatus startProcess() {
+
+        return HttpStatus.OK;
     }
 }
---
+++ src/main/resources/templates/index.js
@@ -0,0 +1,19 @@
+alert("dfdgfgdf");
+/*
+$("#start_button").click(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/start_process",
+        /!*data: {
+            id: $(this).val(), // < note use of 'this' here
+            access_token: $("#access_token").val()
+        },*!/
+        success: function(result) {
+            alert('ok');
+        },
+        error: function(result) {
+            alert('error');
+        }
+    });
+});*/
